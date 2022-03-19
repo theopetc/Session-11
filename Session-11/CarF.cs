4c3d275033e6b37bb348ad28bc7e1affe7b36883
ChrisDomp <dompazis@auth.gr>
2022-03-19 15:05:40 +0200

    Car entity full functional.

--- Session-11/CarF.cs
+++ Session-11/CarF.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
+
 using System.Linq;
 using System.Text;
-using System.Text.Json;
+
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,44 +16,52 @@ namespace Session_11
 {
     public partial class CarF : DevExpress.XtraEditors.XtraForm
     {
-        public const string FILE_NAME = "storage.json";
-        public ServiceCenter _serviceCenter;
-        private Car _car;
-        public CarF()
-        {
-            InitializeComponent();
-        }
 
-        public CarF(ServiceCenter serviceCenter)
-        {
-            InitializeComponent();
-            _serviceCenter = serviceCenter;
-        }
+        State _state;
+        Car _car;
+        BindingSource _bindingSource;
+
+        #region UI
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            this.DialogResult = DialogResult.Cancel;
+
+            this.Close();
         }
         private void btnOK_Click(object sender, EventArgs e)
         {
             Save();
-            //this.Close();
+            this.Close();
+
         }
 
+        #endregion
+
+
+
+        public CarF(State state, Car car, BindingSource bindingSource)
+        {
+            _state = state;
+            _bindingSource = bindingSource;
+            _car = new Car()
+            {
+                ID = car.ID,
+                Brand = car.Brand,
+                Model = car.Model,
+                RegistrationNumber = car.RegistrationNumber,
+            };
+            InitializeComponent();
+        }
 
         private void CarF_Load(object sender, EventArgs e)
         {
-            if (_car == null)
-            {
-                _car = new Car() { Brand = Brands.Reanult };
-                _serviceCenter.Cars.Add(_car);
-                bsCar.DataSource = _car;
-
-            }
-            bsCar.DataSource = _car;
             populateControls();
+            bsCar.DataSource = _car;
             SetDataBindings();
+
         }
+
+
         private void populateControls()
         {
             comboBrand.Properties.Items.Add(Brands.Audi);
@@ -63,18 +71,31 @@ namespace Session_11
             comboBrand.Properties.Items.Add(Brands.Reanult);
             comboBrand.Properties.Items.Add(Brands.Citroen);
             comboBrand.Properties.Items.Add(Brands.Volvo);
+            comboBrand.Properties.Items.Add(Brands.Ferrari);
+            comboBrand.Properties.Items.Add(Brands.Saab);
+            comboBrand.Properties.Items.Add(Brands.Lamborgini);
+            comboBrand.Properties.Items.Add(Brands.Mazda);
 
         }
         private void SetDataBindings()
         {
             ctrlRegNum.DataBindings.Add(new Binding("EditValue", bsCar, "RegistrationNumber", true));
             ctrlModel.DataBindings.Add(new Binding("EditValue", bsCar, "Model", true));
+            comboBrand.DataBindings.Add(new Binding("EditValue", bsCar, "Brand", true));
         }
         private void Save()
         {
-            string json = JsonSerializer.Serialize(_serviceCenter);
-            File.WriteAllText(FILE_NAME, json);
-            DialogResult = DialogResult.OK; 
+            if (_state == State.New)
+            {
+                ((List<Car>)_bindingSource.DataSource).Add(_car);
+
+            }
+            else
+            {
+                var editedItemIndex = ((List<Car>)_bindingSource.DataSource).FindIndex(x => x.ID == _car.ID);
+                ((List<Car>)_bindingSource.DataSource)[editedItemIndex] = _car;
+            }
+
         }
     }
 
